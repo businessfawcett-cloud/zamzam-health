589dc1c7dcb44b19f8a6a7b20d976c653c694b58
Business Fawcett <fawcettbusiness@gmail.com>
2026-05-26 08:41:57 -0600

    Add singleton Site Settings editor to Studio structure

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,7 +1,22 @@
 import type {StructureResolver} from 'sanity/structure'
+import {CogIcon} from '@sanity/icons'
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
     .title('Content')
-    .items(S.documentTypeListItems())
+    .items([
+      S.listItem()
+        .id('siteSettingsSingleton')
+        .title('Site Settings')
+        .icon(CogIcon)
+        .child(
+          S.editor()
+            .id('siteSettings')
+            .schemaType('globalSettings')
+            .documentId('siteSettings')
+        ),
+      ...S.documentTypeListItems().filter(
+        (item) => item.getId() !== 'globalSettings'
+      ),
+    ])
